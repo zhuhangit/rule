4fc161569487c76f02fd76f40c86916fa1c5c2f8
断舍离 <zhuhangit@qq.com>
2024-07-09 21:36:36 +0800

    闲鱼（打开链接）

--- js/xianyu.js
+++ js/xianyu.js
@@ -3,10 +3,9 @@ let accept = $request.Accept || '';
 
 var body = $response.body;
 let headers = $response.headers;
-notify('test','解除限制','点击打开链接','');
 if (accept.indexOf('text/html')>-1) {
   if(url.indexOf('reminderUrl')>-1){
-    const sz = url.split(/?|&|=/);
+    const sz = url.split(/\?|&|=/);
     const reminderUrl = sz[sz.indexOf("reminderUrl")+1];
     //系统通知
     notify('闲鱼','解除限制','点击打开链接',reminderUrl);
