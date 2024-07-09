bf4b816d11533df51a9981685a59bcb3214214cf
断舍离 <zhuhangit@qq.com>
2024-07-09 23:31:24 +0800

    闲鱼（打开链接）

--- js/xianyu.js
+++ js/xianyu.js
@@ -11,8 +11,8 @@ if (url.indexOf('reminderUrl') > -1) {
     const sz = url.split(/\?|&|=/g);
     let reminderUrl = sz[sz.indexOf("reminderUrl") + 1];
     reminderUrl = decodeURIComponent(reminderUrl);
-    console.log('$request',$request)
-    console.log('fRefer',fRefer)
+    console.log(JSON.stringify($request))
+    console.log(fRefer)
     if(fRefer){
         //1.0 系统通知
         notify('闲鱼', '', '点击打开链接', reminderUrl);
