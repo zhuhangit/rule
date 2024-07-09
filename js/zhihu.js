fd35b4e45baa39825a8a9ddb61be7068525f33b6
zhuhangit <32757564+zhuhangit@users.noreply.github.com>
2024-07-09 19:07:33 +0800

    Update zhihu.js

--- js/zhihu.js
+++ js/zhihu.js
@@ -17,13 +17,15 @@ let headers = $response.headers;
 // 判断响应体是否存在
 // 仅处理主页
 if ($response.body && accept.indexOf('text/html')>-1) {  
-    body = body.replaceAll('</BODY>', '</body>').replaceAll(regex, replace_str); 
-    // 定义响应头
-    headers['Content-Security-Policy'] = '*';
-    //test
-    notify('执行成功','$request',JSON.stringify($request),'');
-    body = body.replace('</body>','<h1 style="position: fixed;z-index: 999;left: 0;top: 100px;background-color: #e3c949;width: 100%;height: 40px;line-height: 40px;text-align: center;color: red;letter-spacing: 10px;">512niubi</h1></body>');
-    $done({ headers: headers, body: body, url: url });
+  body = body.replaceAll('</BODY>', '</body>').replaceAll(regex, replace_str); 
+  // 定义响应头
+  headers['Content-Security-Policy'] = '*';
+  
+  //系统通知
+  //notify('执行成功','$request',JSON.stringify($request),'');
+  //body = body.replace('</body>','<h1 style="position: fixed;z-index: 999;left: 0;top: 100px;background-color: #e3c949;width: 100%;height: 40px;line-height: 40px;text-align: center;color: red;letter-spacing: 10px;">512niubi</h1></body>');
+  
+  $done({ headers: headers, body: body, url: url });
 }
 $done({ headers: headers, body: body, url: url });
 
