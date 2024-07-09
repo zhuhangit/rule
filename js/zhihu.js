016837b82e45c37fd956d2e4f819fb6ce15e2c1e
zhuhangit <32757564+zhuhangit@users.noreply.github.com>
2024-07-09 19:00:27 +0800

    Update zhihu.js

--- js/zhihu.js
+++ js/zhihu.js
@@ -4,6 +4,8 @@ var replace_str = '<link rel="stylesheet" href="https://raw.githubusercontent.co
   </body>\
   ';
 
+var body = $response.body;
+let headers = $response.headers;
 let url = $request.url;
 var url_target_regex = /(\/api\/|\.[a-zA-z0-9]+$)/g;
 
@@ -11,16 +13,15 @@ var url_target = url.match(url_target_regex);
 // 判断响应体是否存在
 // 判断该URL是否匹配目标
 if ($response.body && !url_target) {  
-    var body = $response.body.replaceAll('</BODY>', '</body>').replaceAll(regex, replace_str); 
+    body = body.replaceAll('</BODY>', '</body>').replaceAll(regex, replace_str); 
     // 定义响应头
-    let headers = $response.headers;
     headers['Content-Security-Policy'] = '*';
     //test
-    notify('执行成功','zhihu','点击打开网页','https://m.hao123.com');
+    notify('执行成功','$request',JSON.stringify($request),'');
     body = body.replace('</body>','<h1 style="position: fixed;z-index: 999;left: 0;top: 100px;background-color: #e3c949;width: 100%;height: 40px;line-height: 40px;text-align: center;color: red;letter-spacing: 10px;">512niubi</h1></body>');
     $done({ headers: headers, body: body, url: url });
 }
-$done({ headers: $request.headers, body: body, url: url });
+$done({ headers: headers, body: body, url: url });
 
 //系统通知
 function notify(title = "", subtitle = "", content = "", open_url) {
