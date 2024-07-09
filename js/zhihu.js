ac26284eb73066d8ff59db7fdb11778dc8456ca1
zhuhangit <32757564+zhuhangit@users.noreply.github.com>
2024-07-09 18:39:49 +0800

    Update zhihu.js

--- js/zhihu.js
+++ js/zhihu.js
@@ -8,15 +8,26 @@ let url = $request.url;
 var url_target_regex = /(\/api\/|\.[a-zA-z0-9]+$)/g;
 
 var url_target = url.match(url_target_regex);
-if ($response.body) {  // 判断响应体是否存在
-    if (url_target) {  // 判断该URL是否匹配目标
-        $done({ headers: $request.headers, body: body, url: url });
-    } else {
-        var body = $response.body.replaceAll('</BODY>', '</body>').replaceAll(regex, replace_str); 
-    }
+// 判断响应体是否存在
+// 判断该URL是否匹配目标
+if ($response.body && !url_target) {  
+    var body = $response.body.replaceAll('</BODY>', '</body>').replaceAll(regex, replace_str); 
     // 定义响应头
     let headers = $response.headers;
     headers['Content-Security-Policy'] = '*';
+    //test
+    notify('执行成功','zhihu','test','https://hao123.com');
     $done({ headers: headers, body: body, url: url });
 }
 $done({ headers: $request.headers, body: body, url: url });
+
+//系统通知
+function notify(title = "", subtitle = "", content = "", open_url) {
+      let opts = {};
+      if (open_url) opts["open-url"] = open_url;
+      if (JSON.stringify(opts) == "{}") {
+          $notify(title, subtitle, content);
+      } else {
+          $notify(title, subtitle, content, opts);
+      }
+}
