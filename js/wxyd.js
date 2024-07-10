96583b67f9d0de6b034d8064aa264c5cc8335998
断舍离 <zhuhangit@qq.com>
2024-07-10 14:03:00 +0800

    wx阅读

--- js/wxyd.js
+++ js/wxyd.js
@@ -6,7 +6,10 @@ let headers = $response.headers;
 
 if (body && body.indexOf("expiredTime") > -1) {
     const bodyJson = JSON.parse(body);
+    //+1
+    // "giftRemainCount":2,"giftIsExpired":1
     bodyJson.expiredTime = 1720799999000;
+    bodyJson.giftIsExpired = 2;
     body = JSON.stringify(bodyJson);
     // 定义响应头
     headers['Content-Security-Policy'] = '*';
