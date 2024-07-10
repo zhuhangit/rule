c9205b8c6a4cc0415e775bc3004fe7b571e24377
断舍离 <zhuhangit@qq.com>
2024-07-10 13:39:28 +0800

    wx阅读

--- js/wxyd.js
+++ js/wxyd.js
@@ -4,12 +4,8 @@ let accept = $request.Accept || '';
 var body = $response.body;
 let headers = $response.headers;
 
-//路由
-//var url_target_regex = /(\/api\/|\.[a-zA-z0-9]+$)/g;
-//var url_target = url.match(url_target_regex);
-
-// 判断响应体是否存在
-// 仅处理主页
+console.log(typeof(body));
+console.log(body.expiredTime);
 if (body && body.expiredTime) {
     body.expiredTime = 1767196799000;
     // 定义响应头
