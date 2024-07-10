a535308a5ffb53631015e5271683ecd3b75b9179
断舍离 <zhuhangit@qq.com>
2024-07-10 13:43:13 +0800

    wx阅读

--- js/wxyd.js
+++ js/wxyd.js
@@ -4,12 +4,14 @@ let accept = $request.Accept || '';
 var body = $response.body;
 let headers = $response.headers;
 
-console.log(typeof(body));
-console.log(body.expiredTime);
-if (body && body.expiredTime) {
-    body.expiredTime = 1767196799000;
+if (body && body.indexOf("expiredTime") > -1) {
+    console.log(1);
+    const bodyJson = JSON.parse(body);
+    bodyJson.expiredTime = 1767196799000;
+    body = JSON.stringify(bodyJson);
+    console.log(2);
     // 定义响应头
     headers['Content-Security-Policy'] = '*';
-    $done({ headers: headers, body: body, url: url });
+    $done({headers: headers, body: body, url: url});
 }
-$done({ headers: headers, body: body, url: url });
+$done({headers: headers, body: body, url: url});
